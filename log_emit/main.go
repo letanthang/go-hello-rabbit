a129742a4aa1ec54ae17f860b04e96e4bd4e86f7
Le Tan Thang <letanthang@gmail.com>
2019-02-22 15:29:20 +0700

    logs pub/sub

---
+++ log_emit/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func bodyFrom(args []string) string {
+	if len(args) == 1 {
+		return "hello ."
+	}
+
+	// log.Printf("args: %s", args)
+	return strings.Join(args[1:], " ")
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://rabbit:D66z3qm3ynC3@35.186.149.9")
+	defer conn.Close()
+	failOnError(err, "Fail to connect to RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	err = ch.ExchangeDeclare(
+		"logs",
+		"fanout",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare exchange")
+
+	body := bodyFrom(os.Args)
+	err = ch.Publish(
+		"logs", //exchange
+		"",     //routing key
+		false,  //mandatory
+		false,  //immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf("Send a msg: %s", body)
+}
---
+++ log_read/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://rabbit:D66z3qm3ynC3@35.186.149.9")
+	failOnError(err, "Failed to connect RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Fail to create a channel")
+
+	q, err := ch.QueueDeclare(
+		"",    //queue name
+		true,  //durable
+		false, //delete when used
+		true,  //exclusive
+		false, //no wait
+		nil,
+	)
+	failOnError(err, "Fail to declare queue")
+
+	ch.QueueBind(
+		q.Name,
+		"",
+		"logs",
+		false,
+		nil,
+	)
+
+	err = ch.Qos(
+		1,     //prefetch count
+		0,     //prefetch size
+		false, //global
+	)
+	failOnError(err, "Failed to set qos")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	failOnError(err, "Fail to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			dot_count := bytes.Count(d.Body, []byte("."))
+			// log.Printf("Dot count sleep time: %s", dot_count)
+			t := time.Duration(dot_count)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf("[*] Waiting for messages. To exit presss ctrl+c")
+	<-forever
+}
